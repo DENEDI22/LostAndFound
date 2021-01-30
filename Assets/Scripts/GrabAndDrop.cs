0f3a5bb3b7e6ff73b20174b3892e282340119348
Kozhevka <77684218+Kozhevka@users.noreply.github.com>
2021-01-30 15:46:20 +0200

    RayCast works!

--- Assets/Scripts/GrabAndDrop.cs
+++ Assets/Scripts/GrabAndDrop.cs
@@ -1,11 +1,13 @@
 ﻿
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class GrabAndDrop : MonoBehaviour
 {
     private Camera mainCamera;
+    [SerializeField] Transform targetObj;
+    
 
     private void Awake() 
     {
@@ -21,8 +23,16 @@ public class GrabAndDrop : MonoBehaviour
     {
         Vector3 position = gameObject.transform.position;
         RaycastHit hit;
-        Vector3 target = position + mainCamera.transform.forward * range;
-        if (Physics.Raycast(transform.position ,target, out hit))
+        // Vector3 target = (position + mainCamera.transform.forward) * range;
+        // Vector3 target = mainCamera.ScreenToViewportPoint(new Vector3(0.5f, 0.5f, 5f));
+        //Ray ray = mainCamera.ScreenToViewportPoint(mainCamera.transform.forward);
+        Vector3 fromPosition = mainCamera.transform.position;
+        Vector3 toPosition = targetObj.transform.position;
+        Vector3 direction = toPosition - fromPosition;
+        
+
+        Debug.DrawRay(mainCamera.transform.position, direction, Color.red);
+        if (Physics.Raycast(mainCamera.transform.position, direction, out hit))
             return hit.collider.gameObject;
         
     return null;
@@ -33,7 +43,9 @@ public class GrabAndDrop : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        
         Debug.Log(GetMouseHoverObject(5));
+
     }
     
 }
